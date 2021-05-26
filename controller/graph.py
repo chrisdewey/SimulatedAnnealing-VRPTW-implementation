7d4876e36211a37444399e6571564e55cac7f846
Chris Dewey <chris.dewey310@gmail.com>
2021-05-26 16:49:57 +0100

    Changed the destinations to a graph data structure instead of a hash table.

--- controller/graph.py
+++ controller/graph.py
@@ -2,10 +2,29 @@ class Vertex:
     def __init__(self, label):
         self.label = label
 
+    def __str__(self):
+        return "%s" % self.label
+
 
 class Graph:
     def __init__(self):
         self.adjacency_list = {}
+        self.edge_weights = {}
 
     def add_vertex(self, new_vertex):
         self.adjacency_list[new_vertex] = []
+
+    def add_directed_edge(self, from_vertex, to_vertex, weight=1.0):
+        self.edge_weights[(from_vertex, to_vertex)] = weight
+        self.adjacency_list[from_vertex].append(to_vertex)
+
+    def add_undirected_edge(self, vertex_a, vertex_b, weight=1.0):
+        self.add_directed_edge(vertex_a, vertex_b, weight)
+        self.add_directed_edge(vertex_b, vertex_a, weight)
+
+    # def __str__(self):
+    #   return "adjList = %s, && edge weights = %s" % (self.adjacency_list, self.edge_weights)
+
+# https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value     see here, might want to
+# sort dictionary by distance values and replace into an ordered dict, must be careful about ordering the vertex
+# names correctly.
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from model.truck import Truck
 from controller.deliver import deliver
 import model.destination
 import controller.hashing_with_chaining
+import controller.graph
 # it might be better to use hashing with chaining rather than open-addressing... depends how pythons hash() works
 #   and how the data is imported and hashed out (like what key is used??)
 # from controller.load_item_data import load_items
@@ -86,18 +87,48 @@ def load_destinations(input_data, header_lines):
             next(data, None)  # skip specified number of header lines
 
         place_num = 0
-        for place in data:
+
+        for place in data:  # Add all vertices to the graph.
+            address = place[1]
+            vertex = controller.graph.Vertex(address)
+            graph_instance.add_vertex(vertex)
+            # print("okay")
+
+        places.seek(0)  # Return csv reader back to start of file.
+        for i in range(0, header_lines):  # TODO Okayyyyyy i'm not following what's happening here. don't know why it's
+            #                               TODO not the same as the one above, header_lines-1 includes an unwanted row.
+            #                               TODO but i'm not quite sure where to place the places.seek() 0 or 1????
+            next(data, None)  # skip specified number of header lines
+
+        index_counter = 0
+        keys_list = list(graph_instance.adjacency_list)
+#        print(keys_list)
+
+        for place in data:  # Add edges to the graph.
             name = place[0]
             address = place[1]
             i = 2
-            distances = []
-            while place[i] != '' and i < num_col-1:  # create list of all given distance references
-                distances.append(place[i])
+
+            while place[i] != '' and i < num_col-1:
+                # print(graph_instance.adjacency_list[address])
+                if place[i] != '':
+                    vertex_a = keys_list[index_counter]
+                    # print(vertex_a)
+                    vertex_b = keys_list[i-2]
+                    # print(vertex_b)
+                    distance = float(place[i])
+                    graph_instance.add_directed_edge(vertex_a, vertex_b, distance)
+                    # TODO with add_undirected_edge it gives 28 members to each vertex, with directed, it gives 1 then 2
+                    # TODO then 3 etc.. also the last vertex is missing its last connection, b/c there's an index out of
+                    # TODO bounds error stemming from num_col-1 i believe...
+                # print(place[i])
                 i += 1
 
-            new_place = Destination(place_num, name, address, distances)
+            index_counter += 1
+            # new_place = Destination(place_num, name, address, distances
 
-            hash_instance_destinations.insert(new_place, address)  # TODO: change to a graph!!
+            # graph_instance.add_undirected_edge()
+
             # could instead loop though csv for every lookup, but this could be very slow/??
             # Or, and hear me out here -> if the destination[] list is longer, then it's closer to the bottom of the
             # csv file anyway, so just compare their len() and the one with the longer one is daddy. papa.
@@ -118,11 +149,12 @@ def load_trucks(num_of_trucks):
 
 
 def deliver():
-    print(trucks.location)
+    # print(trucks.location)
     for package in trucks.packages:
         # print(hash_instance_packages.search(package))
         # TODO: access packages destination address' distance from current location
-        print()
+        # print()
+        pass
 
 
 # path to the csv file
@@ -131,11 +163,21 @@ data_path_destination = Path("data/WGUPS Distance Table.csv")
 
 # initialize the ChainingHashTable instance
 hash_instance_packages = controller.hashing_with_chaining.ChainingHashTable()
-hash_instance_destinations = controller.hashing_with_chaining.ChainingHashTable()
+# hash_instance_destinations = controller.hashing_with_chaining.ChainingHashTable()
+graph_instance = controller.graph.Graph()
 
 
 load_items(data_path, 8)  # second argument = the number of header lines to skip before processing the data.
 load_destinations(data_path_destination, 8)
+
+# keys_list = list(graph_instance.adjacency_list)
+# key = keys_list[0]
+# print(key)
+# for k, v in graph_instance.adjacency_list.items():
+#     print(k, v)
+
+# print(graph_instance.adjacency_list[key])
+
 
 # user_search()
 
